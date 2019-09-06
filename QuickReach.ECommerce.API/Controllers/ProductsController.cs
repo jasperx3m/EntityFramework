cc14738fcfd69be7fecf352a2d757723556e9e1d
orilla-jasper <jasper.orilla915@gmail.com>
2019-09-06 15:53:28 +0800

    epi

--- QuickReach.ECommerce.API/Controllers/ProductsController.cs
+++ QuickReach.ECommerce.API/Controllers/ProductsController.cs
@@ -25,7 +25,7 @@ namespace QuickReach.ECommerce.API.Controllers
         }
 
         [HttpGet]
-        public IActionResult Get(string search = "", int skip = 0, int count = 10)
+        public IActionResult Get(string search = "", int skip = 0, int count = 100)
         {
             var product = repository.Retrieve(search, skip, count);
             return Ok(product);
